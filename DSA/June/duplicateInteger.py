94b8e83bcf158c9057b3639c6e7abe772975ff5c
Adelaine <ilenhanako@gmail.com>
2024-06-13 02:41:39 +0800

    duplicate Integer 12/6

---
+++ DSA/June/duplicateInteger.py
@@ -0,0 +1,22 @@
+# 217 Contains Duplicates, Easy
+# Given Integer array, nums, return true if any value appears more than once in array, otherwise return False.
+# nums = [1,2,3,4,5] , False
+# nums = [2,3,4,4] , True
+def duplicateInt(nums):
+    #use hashSet, if num doesn't exist in hashSet then add, if exist then return True
+    #initialise hashSet
+    hashSet = set()
+    #iterate through array to find duplicate
+    for num in nums:
+        #check for existence of integer in hashset
+        if num in hashSet:
+            return True
+        #if not exist then add to hashSet
+        hashSet.add(num)
+        return False
+    
+nums = [1,2,3,4,5]
+print(duplicateInt(nums))
+
+#O(n) time, takes O(1) to check for integer by the hash function and O(n) as the loop iterates over each element in the array
+        
